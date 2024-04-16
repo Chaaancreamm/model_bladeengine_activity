604fb5aa8f9a0acc6af25eb686cebbf71d5046c2
Chaaancreamm <cce7990@students.uc-bcf.edu.ph>
2024-04-16 15:48:22 +0800

    UPDATE: fixed wrong code for bookfactory.php

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -5,18 +5,24 @@ namespace Database\Factories;
 use App\Models\Book;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
 class BookFactory extends Factory
 {
-    protected $model = Book::class;
-
-    public function definition()
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
     {
         return [
             'isbn' => $this->faker->isbn13,
-            'title' => $this->faker->sentence,
+            'title' => $this->faker->sentence(4),
             'author' => $this->faker->name,
-            'description' => $this->faker->paragraph,
-            'date_published' => $this->faker->date,
+            'description' => $this->faker->sentence(10),
+            'date_published' => $this->faker->date(),
         ];
     }
 }
